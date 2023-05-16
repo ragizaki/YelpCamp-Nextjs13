e9252e8e300fcbef23dbce7657fb9ff6c213d83a
ragizaki <zmachfj@uwaterloo.ca>
2023-05-15 22:55:21 -0400

    add camp api routes for viewing and creating camps

---
+++ src/app/api/camps/[id]/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  try {
+    const camp = await prisma.campsite.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!camp) {
+      return NextResponse.json({ error: "Camp not found" }, { status: 404 });
+    }
+    return NextResponse.json(camp, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
