f17da70e2f460405adc144393334d58bb008b5b5
ragizaki <zmachfj@uwaterloo.ca>
2023-05-25 19:03:53 -0400

    update card component to display features

--- src/app/Camp.tsx
+++ src/app/Camp.tsx
@@ -1,5 +1,6 @@
-import { Campsite } from "@prisma/client";
+import { type Campsite } from "@prisma/client";
 import Link from "next/link";
+import Image from "next/image";
 
 interface Props {
   camp: Campsite;
@@ -7,14 +8,30 @@ interface Props {
 
 export default function Camp({ camp }: Props) {
   return (
-    <div>
-      <h2>{camp.name}</h2>
-      <p>{camp.description}</p>
-      <p>
-        Location: {camp.city}, {camp.country}
-      </p>
-      <p>Price: ${camp.price}/night</p>
-      <Link href={`/camps/${camp.id}`}>View More</Link>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <Image
+        className="w-full"
+        src={camp.photo}
+        alt="Sunset in the mountains"
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{camp.name}</div>
+        <p className="text-gray-700 text-base">{camp.description}</p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <Link
+          href={`/camps/${camp.id}`}
+          className="inline-block bg-gray-200 hover:bg-gray-300 transition ease-in rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+        >
+          View More
+        </Link>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #travel
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #winter
+        </span>
+      </div>
     </div>
   );
 }
