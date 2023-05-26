984156692527a2eae59802e680aae1acf1ec3c0f
ragizaki <zmachfj@uwaterloo.ca>
2023-05-25 21:55:31 -0400

    add upload.io and google image paths to nextjs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/a/*",
+      },
+      {
+        protocol: "https",
+        hostname: "upcdn.io",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
